be606ae5a38da1fb044be7b26266577590b87aa9
sahidr <sahidr93@gmail.com>
2015-04-21 08:55:08 -0430

    Primera prueba de check_password

--- testclsAccessControl.py
+++ testclsAccessControl.py
@@ -7,14 +7,23 @@ import unittest
 from mdlaccesscontrol import *
 
 class ClsAccessControlTester(unittest.TestCase):
+    #Casos Frontera
+    s0 = "M3ñ0$d8"
+    s1 = "C@d3ñA_ca%a(|3&E$" 
+    s3 = ""
+    
+    #Cadena de caracteres válida
+    s4 = "pa$$w0rD"
     
     #def testClsAccessControl(self):
         #aClsAccessControl = clsAccessControl(object)
        
-    def testClsAccessControlEncript(self):
-        aClsAccessControl = clsAccessControl(object)
-        
+    #def testClsAccessControlEncript(self):
+     #    aClsAccessControl = clsAccessControl(object)
+     #    mensajeEncriptado = aClsAccessControl.encript(s4)
         
     def testClsAccessControlCheck_password(self):
-        aClsAccessControl = clsAccessControl(object)
-        aClsAccessControl.check_password()
+        s4 = "pa$$w0rD"
+        aClsAccessControl = clsAccessControl()
+        mensajeEncriptado = aClsAccessControl.encript(s4)
+        self.assertTrue(aClsAccessControl.check_password(mensajeEncriptado,s4),"Deberia ser true")
